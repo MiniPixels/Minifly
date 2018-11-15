45bd2a6a5e7f13f392b4443442b2e77e9ac783cd
Abraham Khanoyan <khanoyanabraham@gmail.com>
2018-11-15 12:26:51 -0800

    Add new Project

---
+++ Assets/Minifly/Scripts/GameObjectContainer/GameObjectContainer.cs
@@ -0,0 +1,43 @@
+﻿using Assets.Minifly.Scripts.Helpers;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameObjectContainer : MonoBehaviour {
+
+    private Dictionary<string, GameObject> _windows;
+   
+
+	
+	private void Start ()
+    {
+        _windows = new Dictionary<string, GameObject>();
+        FillWindows();
+	}
+
+    private void FillWindows()
+    {
+        _windows.Add(Strings.ShopWindow, GameObject.Find(Strings.ShopWindow));
+        _windows.Add(Strings.GarageWindow, GameObject.Find(Strings.GarageWindow));
+        _windows.Add(Strings.HighscoreWindow, GameObject.Find(Strings.HighscoreWindow));
+        _windows.Add(Strings.SettingsWindow, GameObject.Find(Strings.SettingsWindow));
+        _windows.Add(Strings.MessageWindow, GameObject.Find(Strings.MessageWindow));
+
+        DisableWindows();
+    }
+
+    private void DisableWindows()
+    {
+        foreach (var window in _windows)
+        {
+            window.Value.SetActive(false);
+        }
+    }
+
+    public GameObject GetWindowByName(string name)
+    {
+        return _windows.Where(x => x.Key.Contains(name)).SingleOrDefault().Value;
+    }
+}
---
+++ Assets/Minifly/Scripts/InputManager/InputManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+
+	
+	void Start ()
+    {
+        var game = GameObject.Find("GameobjectContainer").GetComponent<GameObjectContainer>();
+        var window = game.GetWindowByName("ShopWindow");
+        window.SetActive(true);
+	}
+	
+	
+}
